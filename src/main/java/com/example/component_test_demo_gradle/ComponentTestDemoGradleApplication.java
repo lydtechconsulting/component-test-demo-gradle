843d59d88bbc0ec5ee4de0fe39b809dc62a3ec1e
James Powell <james.powell@velopayments.com>
2025-04-30 21:35:55 +0100

    initial commit

---
+++ src/main/java/com/example/component_test_demo_gradle/ComponentTestDemoGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.component_test_demo_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentTestDemoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentTestDemoGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/component_test_demo_gradle/ComponentTestDemoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.component_test_demo_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentTestDemoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
